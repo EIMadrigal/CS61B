9c01439505877633b5b657ca9e45feacc65fb8b6
EIMadrigal <renjingtaofc@163.com>
2020-06-14 17:40:32 +0800

    hw3

---
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -0,0 +1,64 @@
+package hw3.hash;
+
+import edu.princeton.cs.introcs.StdRandom;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestComplexOomage {
+
+    @Test
+    public void testHashCodeDeterministic() {
+        ComplexOomage so = ComplexOomage.randomComplexOomage();
+        int hashCode = so.hashCode();
+        for (int i = 0; i < 100; i += 1) {
+            assertEquals(hashCode, so.hashCode());
+        }
+    }
+
+    /* This should pass if your OomageTestUtility.haveNiceHashCodeSpread
+       is correct. This is true even though our given ComplexOomage class
+       has a flawed hashCode. */
+    @Test
+    public void testRandomOomagesHashCodeSpread() {
+        List<Oomage> oomages = new ArrayList<>();
+        int N = 10000;
+
+        for (int i = 0; i < N; i += 1) {
+            oomages.add(ComplexOomage.randomComplexOomage());
+        }
+
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
+    }
+
+    /*
+     * that shows the flaw in the hashCode function.
+     */
+    @Test
+    public void testWithDeadlyParams() {
+        List<Oomage> deadlyList = new ArrayList<>();
+        int N = 10000;
+
+        // since 256^4,5,6...=0, ...+256^5*z+256^4*x+256^3*c+256^2*v+256*b+n
+        // for each loop, if last 4 params are same, no matter what the front params are,
+        // the hashcode will be the same, will not spread nice, test fail.
+        for (int i = 0; i < N; i += 1) {
+            List<Integer> params = new ArrayList<>();
+            params.add(StdRandom.uniform(255));
+            for (int j = 0; j < 4; ++j) {
+                params.add(j);
+            }
+            deadlyList.add(new ComplexOomage(params));
+        }
+
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
+    }
+
+    /** Calls tests for SimpleOomage. */
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestComplexOomage.class);
+    }
+}
